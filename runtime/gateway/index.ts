bc42a9af2a97873c9e943dcb724c15a934a74816
morris2000 <124549718+morris2000@users.noreply.github.com>
2026-08-07 11:53:07 +0800

    feat: export HTTP gateway

---
+++ runtime/gateway/index.ts
@@ -0,0 +1,24 @@
+export {
+  createGatewayPolicy,
+  GatewayPolicyError,
+  isLoopbackAddress,
+  isRestrictedAddress,
+  validateGatewayTarget,
+  type GatewayPolicy,
+  type GatewayPolicyInput,
+  type LookupAddress,
+  type LookupHost,
+} from "./policy.ts";
+export {
+  createHttpGatewayServer,
+  listenHttpGatewayServer,
+  type GatewayFetch,
+  type HttpGatewayServerOptions,
+  type ListeningHttpGateway,
+} from "./server.ts";
+export {
+  createScramjetProxyPath,
+  parseScramjetProxyUrl,
+  type ScramjetProxyRoute,
+  type ScramjetProxyRouteIds,
+} from "./url.ts";
