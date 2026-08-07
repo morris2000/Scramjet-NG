52dae31cc78a7d5a17fce0734819e7532d3c892a
morris2000 <124549718+morris2000@users.noreply.github.com>
2026-08-07 12:52:53 +0800

    feat: export runtime composition

---
+++ runtime/composition/index.ts
@@ -0,0 +1,9 @@
+export {
+	listenRuntimeCompositionServer,
+	type ListeningRuntimeCompositionServer,
+	type RuntimeCompositionServerOptions,
+} from "./server.ts";
+export {
+	createRuntimeHarnessSource,
+	type RuntimeHarnessSourceOptions,
+} from "./source.ts";
