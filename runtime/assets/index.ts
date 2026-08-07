dfc60adc88d8ad691353a35f6e1c9c3268a66bce
morris2000 <124549718+morris2000@users.noreply.github.com>
2026-08-07 11:28:09 +0800

    feat: expose runtime asset APIs

---
+++ runtime/assets/index.ts
@@ -0,0 +1,25 @@
+export {
+	createRuntimeAssetManifest,
+	normalizeRuntimeAssetPath,
+} from "./manifest.ts";
+export {
+	createScramjetServiceWorkerSource,
+} from "./service-worker.ts";
+export {
+	createRuntimeAssetServer,
+	listenRuntimeAssetServer,
+} from "./server.ts";
+export type {
+	RuntimeAssetDescriptor,
+	RuntimeAssetManifest,
+	RuntimeAssetManifestOptions,
+} from "./manifest.ts";
+export type {
+	ScramjetServiceWorkerOptions,
+} from "./service-worker.ts";
+export type {
+	ListeningRuntimeAssetServer,
+	RuntimeAssetServer,
+	RuntimeAssetServerOptions,
+} from "./server.ts";
+
